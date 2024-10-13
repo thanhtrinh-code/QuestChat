b5c40623cfa19614f3462e013246aafdcdec57de
Thanh Duy Trinh <thanhtrinh501@Thanhs-MacBook-Pro-2.local>
2024-10-12 21:19:35 -0700

    store information and getting nearby places

--- quest-chat/firebase.js
+++ quest-chat/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArTao4AG-Z_SIQfdn0Z27MDqq_7ftVsTQ",
-  authDomain: "quest-chat-17136.firebaseapp.com",
-  projectId: "quest-chat-17136",
-  storageBucket: "quest-chat-17136.appspot.com",
-  messagingSenderId: "962500242585",
-  appId: "1:962500242585:web:286cd0ea8037b9efcfdc08",
-  measurementId: "G-FWNPJJL2D3"
+  apiKey: "AIzaSyArTao4AG-Z_SIQfdn0Z27MDqq_7ftVsTQ", // NEXT_PUBLIC_APIKEY
+  authDomain: "quest-chat-17136.firebaseapp.com", // NEXT_PUBLIC_AUTHDOMAIN
+  projectId: "quest-chat-17136", // NEXT_PUBLIC_PROJECTID
+  storageBucket: "quest-chat-17136.appspot.com", // NEXT_PUBLIC_STORAGEBUCKET
+  messagingSenderId: "962500242585", // NEXT_PUBLIC_MESSAGINGSENDERID
+  appId: "1:962500242585:web:286cd0ea8037b9efcfdc08", // NEXT_PUBLIC_APPID
+  measurementId: "G-FWNPJJL2D3" // NEXT_PUBLIC_MEASUREMENTID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Export the db instance
+export {db};
