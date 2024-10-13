7b06085ba8ab4a29a8b02eea788bcc6385674a01
nguyean <nguyean@uw.edu>
2024-10-12 23:12:53 -0700

    layout

---
+++ quest-chat/app/(root)/dashboard/chats/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatsPage = () => {
+  return (
+    <div>Chat page</div>
+  )
+}
+
+export default ChatsPage;
---
+++ quest-chat/app/(root)/dashboard/requests/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RequestsPage = () => {
+  return (
+    <div>Request page</div>
+  )
+}
+
+export default RequestsPage;
